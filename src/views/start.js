250106246ec3a0d09700a77f092ba53c24b6e300
Nikki Sollid <nikki.sollid@learnify.se>
2017-12-20 12:30:41 +0100

    - visualize start command

--- src/views/start.js
+++ src/views/start.js
@@ -6,16 +6,40 @@ export class Start{
         this.cmdInput = "";
 
         this.replies = [];
+        this.canInput = true;
     }
 
     submit(e){
         if(e.key == "Enter"){
-            let reply = "Command does not exist";
+            let reply = "-bash " + this.cmdInput + ": Command does not exist";
+            this.replies.push(this.name + "-MacBook-Pro:" + this.folder + " " + this.name + "$ " + this.cmdInput);
+
             if(this.cmdInput == "help"){
-                reply = "Start new game by au run --watch<br><br>Load game by au run env --username --watch";
+                this.replies.push("Start new game au run --watch");
+                this.replies.push("Load game au run env --username --watch");
             }
-            this.replies.push(this.cmdInput);
-            this.replies.push(reply);
+
+            else if(this.cmdInput.includes("au run") && this.cmdInput.includes("--watch")){
+                if(this.cmdInput.includes("env")){
+                    this.user = this.cmdInput.split("--")[1];
+                    this.replies.push("Loading "+ this.user);
+                    this.canInput = true;
+                    for(let i = 0;i != 100;i++){
+                        setTimeout(x =>{
+                            this.replies.push("Building " + Math.random().toString(36).substr(2, 36) + " module");
+                            document.getElementById("body").scrollTo(0,document.getElementById("body").scrollHeight);
+                        },Math.floor((Math.random() * 1000) + 1));
+                    }
+                }
+                else {
+                    this.replies.push("Creating");
+                }
+            }
+            
+            else {
+                this.replies.push(reply);
+            }
+
             this.cmdInput = "";
         } else {
             this.cmdInput += e.key;
